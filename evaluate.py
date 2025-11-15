ec11175bf2f755c08e26c631adc3c5a8858eadf0
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2025-11-14 19:01:28 -0600

    added evaluate

---
+++ evaluate.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import torch
+
+device = 0
+if torch.cuda.is_available():
+    device = 0
+    print("Using GPU")
+else:
+    device = "cpu"
+    print("Using CPU")
+
+
+model = YOLO("training_results_weights/best.pt")
+
+
+result = model("test_image.JPG", device = device)   # replace with actual test image
+result[0].show()
+
+
+
