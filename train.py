7e8396c0b82acc60c47cc66bb6e12c86bd7bc38e
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2025-11-14 18:07:54 -0600

    made it to where it works on either gpu or cpu automatically

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 from yolosplitter import YoloSplitter
+import torch
 
 def main():
     model = YOLO("yolov8n.pt")   # Options: yolov8s.pt, yolov8m.pt, yolov8l.pt
@@ -8,12 +9,25 @@ def main():
     # df = ys.from_yolo_dir(input_dir="Hammer", ratio=(0.8, 0.2, 0.1)) # train, val, test ratio
     # ys.save_split(output_dir="potholes_split") # Saves the split dataset with a data.yaml file
 
+    device = 0
+    if torch.cuda.is_available():
+        device = 0
+        print("Using GPU")
+    else:
+        device = "cpu"
+        print("Using CPU")
+
+
+
     model.train(
         data="dataset.yaml",      # path to your YAML
-        epochs=50,                # adjust for your dataset
+        epochs=5,                # adjust for your dataset
         batch=16,
         imgsz=640,
-        device=0                  # GPU; change to 'cpu' if needed
+        device=device,                  # GPU; change to 'cpu' if needed
+        project="training_results",
+        name="run1",
+        exist_ok=True       # ← allows overwriting
     )
 
     # 3. Evaluate on validation set
@@ -22,7 +36,7 @@ def main():
     print(metrics)
 
     # 4. Run inference on a sample image
-    result = model("test_image.JPG")   # replace with actual test image
+    result = model("test_image.JPG", device = device)   # replace with actual test image
     result[0].show()
 
     # 5. Export the model to ONNX format
